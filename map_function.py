d5eab4f223bf7f377c6314656f488ff648e60008
PREMLATASINGH <56335900+PREMLATASINGH@users.noreply.github.com>
2026-02-26 13:34:31 -0500

    added

--- map_function.py
+++ map_function.py
@@ -5,3 +5,5 @@ print(square(-3))
 numbers = [1, 2, 3, 4, 5]
 squared_numbers = list(map(square, numbers))
 print(squared_numbers)
+squared_numbers_lambda = list(map(lambda x: x * x, numbers))
+print(squared_numbers_lambda)
