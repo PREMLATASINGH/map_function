40f392936e48e6f41adb39a0bb1e12cb9d87cbb6
PREMLATASINGH <56335900+PREMLATASINGH@users.noreply.github.com>
2026-02-26 13:48:58 -0500

    added

--- filter_function.py
+++ filter_function.py
@@ -3,3 +3,9 @@ def even(n):
 numbers = [1, 2, 3, 4, 5]
 even_numbers = list(filter(even, numbers))
 print(even_numbers)
+even_numbers_lambda = list(filter(lambda n: n % 2 == 0, numbers))
+print(even_numbers_lambda)
+def odd(n_):
+        return n_ % 2 != 0
+odd_numbers = list(filter(odd, numbers))
+print(odd_numbers)
